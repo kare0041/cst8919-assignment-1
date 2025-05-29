8a0459836da4081701c5dc6ff5e280208cb6ebe2
kare0041 <kare0041@algonquinlive.com>
2025-05-28 23:59:39 -0400

    adding a protected page

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
+from functools import wraps
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
@@ -34,6 +35,14 @@ oauth.register(
 )
 
 # 👆 We're continuing from the steps above. Append this to your server.py file.
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/login")
 def login():
@@ -71,6 +80,11 @@ def logout():
 @app.route("/")
 def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+@app.route("/protected")
+@login_required
+def protected():
+    return render_template("protected.html", session=session.get('user'))
 
 # 👆 We're continuing from the steps above. Append this to your server.py file.
 
